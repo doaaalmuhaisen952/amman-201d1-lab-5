c1e4d407ee4e1a68c8e33496f45205f8b850f480
doaaalmuhaisen952 <almuhaisendoaa@gmail.com>
2019-10-10 13:42:13 +0300

    prob

--- app.js
+++ app.js
@@ -5,13 +5,17 @@ Write a function called sum() that takes in two numbers as arguments and then re
 
 "The sum of 4 and 7 is 11."
 
+
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
+
 function sum(a, b) { //eslint-disable-line
+//given 4,7 should return [11 , the sum of 4 and 7 is 11]
 
+var c= a+b ;
+return  ['The sum of '+ a +'and '+ b +'is '+c ];
 }
-
 // Here is the test for sum(); uncomment it to run it
 // testSum(4, 7);
 
@@ -26,11 +30,12 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
+function multiply(a,b) { //eslint-disable-line
 
+    var c = a*b ;
+    return ['The sum of '+ a +'and '+ b +'is '+ c];
 }
-
-// Here is the test for multiply(); uncomment it to run it
+    // Here is the test for multiply(); uncomment it to run it
 // testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
