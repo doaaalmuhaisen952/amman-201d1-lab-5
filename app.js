9f15f02b7eb3b6226290a9c7b83b939a8055a8c2
doaaalmuhaisen952 <almuhaisendoaa@gmail.com>
2019-10-12 09:21:44 +0300

    problems

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ function sumAndMultiply(a,b,c){  //eslint-disable-line
       var R =sum(H[0],c)
       var D = multiply(a,b)
       var O = multiply(D[0],c)
-     return [R[0],O[0],a+' and '+b + ' and ' + c +' sum to ' + R[0] ,'The product of '+ a +' and '+ b +' and '+ 5 +' is '+ O[0] ];
+     return [R[0],O[0],a+' and '+b + ' and ' + c +' sum to ' + R[0] ,'The product of '+ a +' and '+ b +' and '+ 5 +' is '+ O[0]+' . ' ];
     
     }
     
@@ -78,12 +78,13 @@ IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
-var testArray = [2, 3, 4]; //eslint-disable-line
-
-function sumArray(sumArr) { //eslint-disable-line
+var testArray  = [2, 3, 4];
+function sumArray(testArray) { 
+var firstSum = sum(testArray[0],testArray[1]);
+   var secondSum = sum(firstSum[0],testArray[2]);
+   return [secondSum[0] ,"2,3,4 was passed in as an array of numbers, and 9 is their sum."  ]
 
 }
-
 // Here is the test for sumArray(); uncomment it to run it
 
 // testSumArray(testArray);
@@ -101,8 +102,12 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-function multiplyArray(multArr) { //eslint-disable-line
 
+function multiplyArray(multArr) { //eslint-disable-line
+    var multArr = [2, 3, 4]; //eslint-disable-line
+   var firstMultiply = multiply(multArr[0],multArr[1]);
+   var secondMultiply = multiply(firstMultiply[0],multArr[2]);
+   return [secondMultiply[0], "The numbers 2,3,4 have a product of 24."]
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
@@ -127,13 +132,24 @@ This function should be dynamic, accepting an array of any length.
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyAnyArray() function and see if the test passes.*/
 
 // Write your code here
-var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
 
-function multiplyAnyArray(dynamicArray) { //eslint-disable-line
+//function multiplyAnyArray(dynamicArray) { //eslint-disable-line
 
+function multiplyAnyArray(testDynamicArray) { //eslint-disable-line
+    var testDynamicArray = [1, 2, 3, 4, 5]; //eslint-disable-line
+
+   var testMultiply = multiply(testDynamicArray[0],testDynamicArray[1]);
+   var secondTestMultiply = multiply(testMultiply[0],testDynamicArray[2]);
+   var theredTestMultiply = multiply(secondTestMultiply[0],testDynamicArray[3]);
+   var forthTestMultiply = multiply(theredTestMultiply[0],testDynamicArray[4]);
+   return [forthTestMultiply[0], "The numbers 1,2,3,4,5 have a product of 120."]
 }
+// Collapse
 
-// Here is the test for multiplyArray(); uncomment it to run it
+
+
+
+// Here is the tesor multiplyArray(); uncomment it to run it
 // testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
